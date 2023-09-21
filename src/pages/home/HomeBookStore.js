1142586c738d8c5ff393db630b7791969e962c75
khoanguyen23 <khoahoang151@gmail.com>
2023-09-21 19:51:04 +0700

    [add] setup project

---
+++ src/pages/home/HomeBookStore.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import MetaTags from "react-meta-tags";
+import LayoutTwo from "../../layouts/LayoutTwo";
+
+
+const HomeBookStore = () => {
+  return (
+    <Fragment>
+      <MetaTags>
+        <title>Flone | Book Store Home</title>
+        <meta
+          name="description"
+          content="Book store home of flone react minimalist eCommerce template."
+        />
+      </MetaTags>
+      <LayoutTwo>
+       
+      </LayoutTwo>
+    </Fragment>
+  );
+};
+
+export default HomeBookStore;
